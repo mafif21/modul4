dabfbf8e52fd0cfee86daaa487b68217a7e374a3
Muhammad Nurul Afif Maliki <afif.maliki21@gmail.com>
2023-11-14 20:02:14 +0700

    UI Finish

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# Jurnal Modul 3
+
+### Deskripsi
+
+Halo semuanya, pada jurnal kali ini kalian diwajibkan untuk membuat sebuah Login Management System menggunakan bahasa PHP dengan database Mysql
+
+### Pengetahuan
+
+- MVC Arsitektur
+- Pengambilan Data
+- Validasi User
+
+### Struktur Aplikasi
+
+- Model : Digunakan untuk mendefinisikan database
+- View : Digunakan untuk semua tampilan aplikasi ( UI )
+- Controller : Digunakan untuk logika aplikasi
+- Aset : Untuk menyimpan semua gambar / icon ( abaikan saja )
---
+++ view/Register.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap demo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+  <div class="container d-flex min-vh-100 justify-content-center align-items-center">
+    <div class="card" style="width: 30rem;">
+      <img src="../assets/photo.jpeg" class="card-img-top">
+      <div class="card-body">
+        <div class="title mb-4">
+          <h3>Login Page</h3>
+        </div>
+
+        <div class="user-input">
+
+          <!-- perhatikan bagian ini -->
+          <form>
+            <div class="mb-3">
+              <label for="username" class="form-label">Username</label>
+              <input type="text" class="form-control" id="username" name="username" placeholder="your username...">
+            </div>
+            <div class="mb-3">
+              <label for="email" class="form-label">Email address</label>
+              <input type="email" class="form-control" id="email" name="email" placeholder="your email...">
+            </div>
+            <div class="mb-3">
+              <label for="password" class="form-label">Password</label>
+              <input type="password" class="form-control" id="password" name="password" placeholder="your password...">
+            </div>
+            <div class="mb-3">
+              <label for="passwordValidate" class="form-label">Validate Password</label>
+              <input type="password" class="form-control" id="passwordValidate" name="passwordValidate"
+                placeholder="validate password...">
+            </div>
+            <button type="submit" name="register" class="btn btn-primary w-100">Register</button>
+          </form>
+          <!-- perhatikan bagian ini -->
+
+          <p class="pt-2 fs-6 text-center">
+            Already have an account?
+            <a href="./Login.php" class="text-decoration-none">Login Account</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
