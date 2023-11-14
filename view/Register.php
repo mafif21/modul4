463f93f09abd18b0a98d5123a76e0793495e7a7b
Muhammad Nurul Afif Maliki <afif.maliki21@gmail.com>
2023-11-14 20:13:22 +0700

    typo

--- view/Register.php
+++ view/Register.php
@@ -4,9 +4,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Bootstrap demo</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <title>Register</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
 <body>
@@ -15,7 +14,7 @@
       <img src="../assets/photo.jpeg" class="card-img-top">
       <div class="card-body">
         <div class="title mb-4">
-          <h3>Login Page</h3>
+          <h3>Register Page</h3>
         </div>
 
         <div class="user-input">
@@ -36,8 +35,7 @@
             </div>
             <div class="mb-3">
               <label for="passwordValidate" class="form-label">Validate Password</label>
-              <input type="password" class="form-control" id="passwordValidate" name="passwordValidate"
-                placeholder="validate password...">
+              <input type="password" class="form-control" id="passwordValidate" name="passwordValidate" placeholder="validate password...">
             </div>
             <button type="submit" name="register" class="btn btn-primary w-100">Register</button>
           </form>
@@ -51,8 +49,7 @@
       </div>
     </div>
   </div>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
   </script>
 </body>
 
